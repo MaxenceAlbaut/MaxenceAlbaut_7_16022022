83ba692a5ee9ecae747d04cb12858ba35882cc2a
MaxenceAlbaut <maxencealbaut@gmail.com>
2022-02-16 15:21:51 +0100

    integration des premiers composants

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,5 +3,26 @@
         <h1>Login</h1>
         <br>
         <img style="width: 330px; height: 330px" alt="Groupomania logo" src="../assets/icon-above-font.png" />
+        <LoginForm></LoginForm>
+        <NavBar></NavBar>
     </div>
 </template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+    name: 'Login',
+    components: {LoginForm, NavBar}
+}
+</script>
+
+<style>
+
+img {
+    width: 330px;
+    height: 330px
+}
+
+</style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -2,6 +2,27 @@
     <div class="signup">
         <h1>Signup</h1>
         <br>
-        <img style="width: 330px; height: 330px" alt="Groupomania logo" src="../assets/icon-above-font.png" />
+        <img alt="Groupomania logo" src="../assets/icon-above-font.png" />
+        <SignupForm></SignupForm>
+        <NavBar></NavBar>
     </div>
 </template>
+
+<script>
+import SignupForm from '@/components/SignupForm.vue'
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+    name: 'Login',
+    components: {SignupForm, NavBar}
+}
+</script>
+
+<style>
+
+img {
+    width: 330px;
+    height: 330px
+}
+
+</style>
