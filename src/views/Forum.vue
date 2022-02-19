9d99a2645d54f74120dea9dca59ad20b859f0300
MaxenceAlbaut <maxencealbaut@gmail.com>
2022-02-19 17:02:29 +0100

    Ajout des premiers composants

--- src/views/Forum.vue
+++ src/views/Forum.vue
@@ -1,5 +1,24 @@
 <template>
     <div class="forum">
         <h1>Forum</h1>
+        <PageHeader></PageHeader>
+        <PostArticle></PostArticle>
+        <ActivityFeed></ActivityFeed>
+
     </div>
 </template>
+
+<script>
+import PageHeader from '@/components/PageHeader.vue'
+import PostArticle from '@/components/PostArticle.vue'
+import ActivityFeed from '@/components/ActivityFeed.vue'
+
+export default {
+    name: 'Forum',
+    components: {PageHeader, PostArticle, ActivityFeed}
+}
+
+</script>
+
+<style scoped>
+</style>
