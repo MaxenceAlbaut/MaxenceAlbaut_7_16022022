45dbed8963f3456e24d8bf1663e69acbf1e6eff8
MaxenceAlbaut <maxencealbaut@gmail.com>
2022-02-19 16:59:01 +0100

    Ajout du fetch de l'api lors de la validation du formulaire

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,23 +2,45 @@
 
 <form>
     <label for="email">Email:</label><br>
-    <input type="text" id=email name="email"><br>
+    <input type="text" id=email name="email" ref="useremail"><br> 
     <label for="password">Password:</label><br>
-    <input type="text" id=password name="password"><br>
-    <FormButton text="Se connecter"></FormButton>
+    <input type="password" id=password name="password" ref="userpassword"><br>
+    <FormButton text="Se connecter" @click="login"></FormButton>
 </form>
-
 
 </template>
 
 
-
 <script>
-import FormButton from '@/components/FormButton.vue'
+import FormButton from '@/components/FormButton.vue';
 
 export default {
     name: 'LoginForm',
-    components: {FormButton}
+    components: {FormButton},
+    methods: {
+        login(event) {
+            var post = {    // CREATION DU PAYLOAD A ENVOYER
+                email: this.$refs.useremail.value,
+                password: this.$refs.userpassword.value
+            };
+            console.log("payload:" + JSON.stringify(post));
+            fetch('http://localhost:4000/api/auth/login', {     // CREATION DE LA REQUETTE A ENVOYER A L'API
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(post),
+            })
+            .then(response => response.json())
+                .then(data => {
+                    console.log("Response : ", data);
+                    // REDIRIGER L'UTILISATEUR VERS LA PAGE FORUM + UTILISER LE TOKEN ?
+                })
+            .catch(error => {
+                console.error('Error :', error);
+            });
+        }
+    }
 }
 
 </script>
@@ -28,5 +50,8 @@ export default {
 <style scoped>
 form {
     background-color: lightblue;
+    border-radius: 10px;
+    width: 40%;
+    margin: auto;
 }
 </style>
