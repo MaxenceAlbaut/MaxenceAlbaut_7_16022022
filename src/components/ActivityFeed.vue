06022368219660d59d32ab5c2ad43cea67530bc3
MaxenceAlbaut <maxencealbaut@gmail.com>
2022-02-23 06:52:29 +0100

    ajout du fetch des articles

--- src/components/ActivityFeed.vue
+++ src/components/ActivityFeed.vue
@@ -1,9 +1,9 @@
 <template>
 
 <div class="activityFeedContainer">
-    <!-- v-for .... -->
-    <TheArticle></TheArticle>
-    <TheArticle></TheArticle>
+
+    <TheArticle v-for="article in articleArray" :articleId="article.id"> {{ article }}</TheArticle>
+
 </div>
 
 
@@ -14,8 +14,43 @@ import TheArticle from '@/components/TheArticle.vue'
 
 export default {
     name: 'ActivityFeed',
-    components: {TheArticle}
+    components: {TheArticle},
+    data() {
+        return {
+            articleArray: []
+        }
+    },
+    mounted() {
+        let jwt = window.sessionStorage.getItem('jwt');
+        let fetchurl = "http://localhost:4000/api/article/";
+
+        fetch(fetchurl, {     // CREATION DE LA REQUETTE A ENVOYER A L'API
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${jwt}`
+                }
+            })
+            .then(response => response.json())
+                .then(data => {
+                    // Met a jour la data de l'objet de facon reactive
+
+                    for (let i = 0 ; i < data.result.length ; i++) {
+                        console.log(data.result[i]);
+                        this.articleArray.push(data.result[i]);
+                    }
+                })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 }
+
+
+
+
+
 </script>
 
 <style scoped>
