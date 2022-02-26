376dc2ab7a62946fe12f83b4e3ca7cb5bfe554b6
MaxenceAlbaut <maxencealbaut@gmail.com>
2022-02-26 09:39:58 +0100

    first commit

---
+++ src/components/PostCommmentButton.vue
@@ -0,0 +1,28 @@
+<template>
+
+<button type="button">Commenter !</button>
+
+</template>
+
+
+
+<script>
+
+export default {
+    name: 'PostCommentButton'
+}
+
+</script>
+
+
+
+<style scoped>
+button {
+    background-color: darkblue;
+    height: 30px;
+    width: 120px;
+    font-weight: bold;
+    color: white;
+
+}
+</style>
