dad1f0dedef9d837daff96b8c06fba775cb594b4
MaxenceAlbaut <maxencealbaut@gmail.com>
2022-02-27 13:51:16 +0100

    ajout de multer pour la route signup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const userCtrl = require('../controllers/user');
+const multer = require('../middleware/multer-config');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', multer, userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.get('/:id', auth, userCtrl.getUser);
 
