608ed3025a8155468b087d426b58c7ba0d22df97
MaxenceAlbaut <maxencealbaut@gmail.com>
2022-02-27 13:55:19 +0100

    modification de la requette pour inclure une image

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,16 +1,18 @@
 <template>
 
-<form>
+<form id="signupForm">
     <label for="prenom">Prenom:</label><br>   
-    <input type="text" id=prenom name="prenom" ref="userfirstname"><br>
+    <input type="text" id="prenom" name="prenom" ref="userfirstname"><br>
     <label for="nom">Nom:</label><br>
-    <input type="text" id=nom name="nom" ref="userlastname"><br>
+    <input type="text" id="nom" name="nom" ref="userlastname"><br>
     <label for="email">Email:</label><br>
-    <input type="text" id=email name="email" ref="useremail"><br>
+    <input type="text" id="email" name="email" ref="useremail"><br>
     <label for="password">Password:</label><br>
-    <input type="password" id=password name="password" ref="userpassword"><br>
+    <input type="password" id="password" name="password" ref="userpassword"><br>
     <label for="confpw">Confirm password:</label><br>
-    <input type="password" id=confpw name="confpw" ref="userconfpw"><br>
+    <input type="password" id="confpw" name="confpw" ref="userconfpw"><br>
+    <label for="file">Ajouter une photo</label><br>
+    <input type="file" class="input-file" id="file" name="file" ref="userimg"><br>
     <FormButton text="S'inscrire" @click="signup"></FormButton>
 </form>
 
@@ -28,20 +30,24 @@ export default {
     methods: {
         signup(event) {
             // Verification de la concordance des mots de passe
+
             if (this.$refs.userpassword.value === this.$refs.userconfpw.value) {
-                var post = {    // CREATION DU PAYLOAD A ENVOYER
-                prenom: this.$refs.userfirstname.value,
-                nom: this.$refs.userlastname.value,
-                email: this.$refs.useremail.value,
-                password: this.$refs.userpassword.value
-                };
-                console.log("payload:" + JSON.stringify(post));
+                
+
+                console.log(this.$refs.userfirstname.value);
+
+                var postFormData = new FormData(); // CREATION DU PAYLOAD A ENVOYER
+                postFormData.append('prenom', this.$refs.userfirstname.value);
+                postFormData.append('nom', this.$refs.userlastname.value);
+                postFormData.append('email', this.$refs.useremail.value);
+                postFormData.append('password', this.$refs.userpassword.value);
+                postFormData.append('file', this.$refs.userimg.files[0]);
+
+                
+                console.log("payload:" + postFormData);
                 fetch('http://localhost:4000/api/auth/signup', {     // CREATION DE LA REQUETTE A ENVOYER A L'API
                     method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(post),
+                    body: postFormData
                 })
                 .then(response => response.json())
                     .then(data => {
@@ -77,4 +83,9 @@ label {
     margin-top: 20px;
     font-weight: bold;
 }
+
+.input-file{
+    margin: auto;
+}
+
 </style>
