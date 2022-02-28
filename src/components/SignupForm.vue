5a66447a25d6ed013e0dfefbae1b0c180207f134
MaxenceAlbaut <maxencealbaut@gmail.com>
2022-02-28 16:12:35 +0100

    Ajout de la verification des champs du formulaire via des regex, ajout de messages d'erreurs

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -3,16 +3,21 @@
 <form id="signupForm">
     <label for="prenom">Prenom:</label><br>   
     <input type="text" id="prenom" name="prenom" ref="userfirstname"><br>
+    <div v-if="fieldserror.prenom" class="errormsg">Prenom incorrecte</div>
     <label for="nom">Nom:</label><br>
     <input type="text" id="nom" name="nom" ref="userlastname"><br>
+    <div v-if="fieldserror.nom" class="errormsg">Nom incorrecte</div>
     <label for="email">Email:</label><br>
     <input type="text" id="email" name="email" ref="useremail"><br>
+    <div v-if="fieldserror.email" class="errormsg">Email incorrecte</div>
     <label for="password">Password:</label><br>
     <input type="password" id="password" name="password" ref="userpassword"><br>
     <label for="confpw">Confirm password:</label><br>
     <input type="password" id="confpw" name="confpw" ref="userconfpw"><br>
+    <div v-if="fieldserror.password" class="errormsg">Mot de passe different</div>
     <label for="file">Ajouter une photo</label><br>
     <input type="file" class="input-file" id="file" name="file" ref="userimg"><br>
+    <div v-if="fieldserror.img" class="errormsg">Veuillez ajouter une image (png, jpg, jpeg)</div>
     <FormButton text="S'inscrire" @click="signup"></FormButton>
 </form>
 
@@ -27,15 +32,45 @@ import FormButton from '@/components/FormButton.vue'
 export default {
     name: 'SignupForm',
     components: {FormButton},
+    data() {
+        return {
+            fieldserror: {
+                prenom: false,
+                nom: false,
+                email: false,
+                password: false,
+                img: false
+            }
+        }
+    },
     methods: {
         signup(event) {
-            // Verification de la concordance des mots de passe
 
-            if (this.$refs.userpassword.value === this.$refs.userconfpw.value) {
+            // Verification des champs
+            let isValid = true;
+            if (!/^[a-z ,.'-]+$/i.test(this.$refs.userfirstname.value)) {
+                this.fieldserror.prenom = true;
+                isValid = false;
+            } else {this.fieldserror.prenom = false;}
+            if (!/^[a-z ,.'-]+$/i.test(this.$refs.userlastname.value)) {
+                this.fieldserror.nom = true;
+                isValid = false;
+            } else {this.fieldserror.nom = false;}
+            if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(this.$refs.useremail.value)) {
+                this.fieldserror.email = true;
+                isValid = false;
+            } else {this.fieldserror.email = false;}
+            if (this.$refs.userpassword.value != this.$refs.userconfpw.value) {
+                this.fieldserror.password = true;
+                isValid = false;
+            } else {this.fieldserror.password = false;}
+            if (!/.(?:jpg|jpeg|png)$/.test(this.$refs.userimg.value)) {
+                this.fieldserror.img = true;
+                isValid = false;
+            } else {this.fieldserror.img = false;}
+
+            if (isValid) {
                 
-
-                console.log(this.$refs.userfirstname.value);
-
                 var postFormData = new FormData(); // CREATION DU PAYLOAD A ENVOYER
                 postFormData.append('prenom', this.$refs.userfirstname.value);
                 postFormData.append('nom', this.$refs.userlastname.value);
@@ -52,16 +87,16 @@ export default {
                 .then(response => response.json())
                     .then(data => {
                         console.log("Response : ", data);
-                        // REDIRIGER L'UTILISATEUR VERS LA PAGE FORUM + UTILISER LE TOKEN ?
+                        if (data.message == 'Utilisateur cree') {
+                                this.$router.push({
+                                name: 'Login'
+                            });
+                        }
                     })
                 .catch(error => {
                     console.error('Error :', error);
                 });
             }
-            else {
-                window.alert("passwords dont match !");
-            }
-
         }
     }
 }
@@ -86,6 +121,14 @@ label {
 
 .input-file{
     margin: auto;
+}
+
+.errormsg {
+    color: black;
+    margin: 10px auto 0 auto;
+    width: 40%;
+    background-color: rgb(255, 184, 184);
+    border: 2px solid rgb(90, 0, 0)
 }
 
 </style>
