bbc552c42b4f2bbef0ca7a048f45a123a70257a1
MaxenceAlbaut <maxencealbaut@gmail.com>
2022-02-28 16:05:38 +0100

    ajout de messages d'erreurs

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,9 +2,11 @@
 
 <form>
     <label for="email">Email:</label><br>
-    <input type="text" id="email" name="email" ref="useremail"><br> 
+    <input type="text" id="email" name="email" ref="useremail"><br>
+    <div v-if="emailerror" class="errormsg">Email incorrecte</div>
     <label for="password">Password:</label><br>
     <input type="password" id="password" name="password" ref="userpassword"><br>
+    <div v-if="pwerror" class="errormsg">Mot de passe incorrecte</div>
     <FormButton text="Se connecter" @click="login"></FormButton>
 </form>
 
@@ -18,6 +20,12 @@ import FormButton from '@/components/FormButton.vue';
 export default {
     name: 'LoginForm',
     components: {FormButton},
+    data() {
+        return {
+            emailerror: false,
+            pwerror: false
+        }
+    },
     methods: {
         login(event) {
             var post = {    // CREATION DU PAYLOAD A ENVOYER
@@ -39,10 +47,19 @@ export default {
                     let sessStorage = window.sessionStorage;
                     sessStorage.setItem('jwt', `${data.token}`);
                     sessStorage.setItem('userId', `${data.userId}`);
-                    if (!data.error) { // SI L'utilisateur est bien authentifie : Redirige vers la page forum
+                    if (!data.message) { // SI L'utilisateur est bien authentifie : Redirige vers la page forum
                         this.$router.push({
                             name: 'Forum'
                         });
+                    }
+                    // Affiche les erreurs pour l'utilisateur
+                    else if (data.message == 'User not found') { 
+                        this.emailerror = true;
+                        this.pwerror = false;
+                    }
+                    else if (data.message == 'Incorrect password') {
+                        this.emailerror = false;
+                        this.pwerror = true;
                     }
                 })
             .catch(error => {
@@ -68,5 +85,13 @@ label {
     display: inline-block;
     margin-top: 20px;
     font-weight: bold;
+}
+
+.errormsg {
+    color: black;
+    margin: 10px auto 0 auto;
+    width: 40%;
+    background-color: rgb(255, 184, 184);
+    border: 2px solid rgb(90, 0, 0)
 }
 </style>
