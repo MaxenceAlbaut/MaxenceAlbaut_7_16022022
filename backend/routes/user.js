082fb9225f85625603fe49d1a1239bb8212d8f47
MaxenceAlbaut <maxencealbaut@gmail.com>
2022-03-01 18:09:45 +0100

    ajout de la route delete

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,5 +8,6 @@ const multer = require('../middleware/multer-config');
 router.post('/signup', multer, userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.get('/:id', auth, userCtrl.getUser);
+router.delete('/:id', auth, userCtrl.deleteUser);
 
 module.exports = router;
