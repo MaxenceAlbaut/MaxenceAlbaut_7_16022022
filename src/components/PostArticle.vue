6a51af3c48808bfd78de5a679a848bd256cf0965
MaxenceAlbaut <maxencealbaut@gmail.com>
2022-03-02 20:01:42 +0100

    replaced input with textarea, added css

--- src/components/PostArticle.vue
+++ src/components/PostArticle.vue
@@ -1,9 +1,7 @@
 <template>
 
 <div class="postArticle">
-    <div class="PostContentContainer">
-        <input type="text" class="PostArticleContent" placeholder="Redigez votre article ici..." ref="content">
-    </div>
+    <textarea class="PostArticleContent" placeholder="Redigez votre article ici..." ref="content"></textarea>
     <PostArticleButton @click="postArticle"></PostArticleButton>
 </div>
 
@@ -17,56 +15,64 @@ export default {
     components: {PostArticleButton},
     methods: {
         postArticle(event) {
-            let jwt = window.sessionStorage.getItem('jwt');
-            let userId = window.sessionStorage.getItem('userId');
+            if (this.$refs.content.value) {
+                let jwt = window.sessionStorage.getItem('jwt');
+                let userId = window.sessionStorage.getItem('userId');
 
-            var post = {    // CREATION DU PAYLOAD A ENVOYER
-                user_id: userId,
-                text_content: this.$refs.content.value
-            };
+                var post = {    // CREATION DU PAYLOAD A ENVOYER
+                    user_id: userId,
+                    text_content: this.$refs.content.value
+                };
 
-            fetch('http://localhost:4000/api/article/', {     // CREATION DE LA REQUETTE A ENVOYER A L'API
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${jwt}`
-                },
-                body: JSON.stringify(post),
-            })
-            .then(response => response.json())
-                .then(data => {
-                    console.log("Response : ", data);
-                    this.$router.go() // rafraichi la page
-
+                fetch('http://localhost:4000/api/article/', {     // CREATION DE LA REQUETTE A ENVOYER A L'API
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${jwt}`
+                    },
+                    body: JSON.stringify(post),
                 })
-            .catch(error => {
-                console.error('Error :', error);
-            });
+                    .then(response => response.json())
+                        .then(data => {
+                            console.log("Response : ", data);
+                            this.$router.go() // rafraichi la page
 
-
+                        })
+                    .catch(error => {
+                        console.error('Error :', error);
+                    });
+            }
         }
     }
-    
 }
 </script>
 
 <style scoped>
 
 .postArticle {
-    width: 90%;
-    border: 2px solid red;
+    width: 96%;
+    border: 1px solid rgb(184, 184, 184);
     margin-bottom: 30px;
     margin: auto;
+    border-radius: 6px;
+    box-shadow: 0 2px 10px -6px rgb(105, 105, 105);
+    background-color: rgb(241, 251, 255);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
-.PostContentContainer {
-    display: flex;
-    justify-content: space-around;
-}
 
 .PostArticleContent {
-    height: 100px;
-    width: 300px;
+    margin-top: 16px;
+    border-radius: 10px;
+    border: 1px solid rgb(184, 184, 184);
+    background-color: rgb(255, 255, 255);
+    height: 60px;
+    width: 80%;
+    min-width: 340px;
+    text-overflow: wrap;
+    padding: 6px 4px 4px 6px;
 }
 
 .addImg {
